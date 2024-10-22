0ca48771ca75386ddc6feb1690e434bf29ee2d3d
rimurootto <160866529+rimurootto@users.noreply.github.com>
2024-10-22 08:56:01 -0300

    Add files via upload

---
+++ Luiz-Pedro-3tri/README.md
@@ -0,0 +1,1 @@
+# 3Tri-2E-RAFAEL-MANUELA
---
+++ Luiz-Pedro-3tri/main.js
@@ -0,0 +1,5 @@
+function criarCartão(categortia,pergunta,resposta){
+    console.log(categortia,pergunta,resposta)
+}
+
+criarCartão('Biologia','Qual a função da mitrocondrea', 'Produção de energia (ATP)');
